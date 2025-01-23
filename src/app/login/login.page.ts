a86b3e33a955f812e33654d976d147965c0bfd83
v4r13d3s <20233L001041@utcv.edu.mx>
2025-01-23 09:02:27 -0600

    Implementación de validaciones, email y password

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,10 +8,47 @@ import { Component, OnInit } from '@angular/core';
   standalone: false,
 })
 export class LoginPage implements OnInit {
+  // Con TypeScript 
 
-  constructor() { }
+  email: String = ''; // Inicialización directa, las comillas representan una cadena vaciac:\Users\Oscar\Desktop\Práctica Puerta Inteligente 
+  password: String = ''; 
+  //No es tan recomendada esta forma para inicializar una variable
+  constructor() {
+    // Segunda manera para inicializar una variable
+   }
 
   ngOnInit() {
   }
 
+  login(form: NgForm){
+
+
+    console.log('email: ', this.email);
+    console.log('password: ', this.password);
+
+    console.log(form.value);
+    
+    //Validar formulario
+    console.log('valid', form.valid)
+
+    //Si form es inválido
+    //Console log ('Todos los campos son requeridos)
+    //Return
+
+    if(!form.valid) {
+      console.log("Todos los campos son requeridos");
+      return; //Con el return le digo que ya no continue con la ejecución
+    }
+
+    if(this.email == 'Admin' && this.password == 'Admin'){
+      console.log('El Correo y la contraseña es correcto');
+    } else {
+      console.log('El Correo y la contraseña es incorrecto');
+    }
+    //console.log('Email: ', this.email); 
+    //Si usuario=admin y password=admin
+    //Entonces console.log('Login Correcto')
+    //Sino console.log('Login incorrecto')
+  }
 }
+
