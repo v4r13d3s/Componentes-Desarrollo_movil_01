199e61874bb6ec6219b1c26292a94087dab68e65
v4r13d3s <20233L001041@utcv.edu.mx>
2025-01-29 17:57:27 -0600

    Actualización, implementación de chat

--- src/app/chat/chat.page.ts
+++ src/app/chat/chat.page.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   standalone: false,
 })
 export class ChatPage implements OnInit {
+  //El any no es recomendable usarlo, pero como no sabemos que tipos de datos son, vamos a poner any
+  chats: any[] = [1, 2, 3, 4, 5, 6, 7, 8, 9]; //Y en mi chat.page.html debo de declarar mi chat de los chats
 
   constructor() { }
 
